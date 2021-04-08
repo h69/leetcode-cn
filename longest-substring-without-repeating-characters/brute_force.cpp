5a0480912721524489d6bc7da93c265f62c6e78b
h69 <517364792@qq.com>
2021-04-08 21:11:15 +0800

    update

--- longest-substring-without-repeating-characters/brute_force.cpp
+++ longest-substring-without-repeating-characters/brute_force.cpp
@@ -9,6 +9,7 @@ public:
 
     for (int i = 0; i < s.length(); i++) {
       string substr;
+
       for (int j = i; j < s.length(); j++) {
         substr = s.substr(i, j - i + 1);
         if (substr.find(s[j + 1]) != string::npos) {
